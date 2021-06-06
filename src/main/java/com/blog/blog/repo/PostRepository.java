25aea498185e68b24ff6adbe0badc6ae9ad7d188
xwOwiDbN <bogdan.kalika@gmail.com>
2021-06-06 23:52:54 +0300

    blog

---
+++ src/main/java/com/blog/blog/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.blog.blog.repo;
+
+import com.blog.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
